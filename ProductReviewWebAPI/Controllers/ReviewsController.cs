5599d494faf844ed0d71dc9ea7b8c4d5772ccd58
ChaseLoeffler <chaselo609@gmail.com>
2023-09-21 17:23:46 -0400

    Completed and tested method CreateReview in ReviewsController.

--- ProductReviewWebAPI/Controllers/ReviewsController.cs
+++ ProductReviewWebAPI/Controllers/ReviewsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ProductReviewWebAPI.Data;
 using ProductReviewWebAPI.DTOs;
+using ProductReviewWebAPI.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -41,13 +42,16 @@ namespace ProductReviewWebAPI.Controllers
 
         // POST api/<ReviewsController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult CreateReview([FromBody] Review review)
         {
+            _context.Reviews.Add(review);
+            _context.SaveChanges();
+            return StatusCode(201, review);
         }
 
         // PUT api/<ReviewsController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void UpdateReview(int id, [FromBody] string value)
         {
         }
 
