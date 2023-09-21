eef7c8889a806456573b5ce4433b034a3de2edee
ChaseLoeffler <chaselo609@gmail.com>
2023-09-21 19:38:30 -0400

    Created and Tested both GetProductById method and CreateProduct method.

--- ProductReviewWebAPI/Controllers/ProductsController.cs
+++ ProductReviewWebAPI/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ProductReviewWebAPI.Data;
 using ProductReviewWebAPI.DTOs;
+using ProductReviewWebAPI.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -16,11 +17,12 @@ namespace ProductReviewWebAPI.Controllers
         {
             _context = context;
         }
-        // GET: api/<ProductsController>
+        // GET: api/Products or api/Products?maxprice=5
         [HttpGet]
         public IActionResult GetAllProducts([FromQuery] double? maxprice)
         {
-            if(maxprice != null)
+
+            if (maxprice != null)
             {
                 var pricedProducts = _context.Products.Select(p => new ProductDTO
                 {
@@ -35,7 +37,7 @@ namespace ProductReviewWebAPI.Controllers
 
                     }).ToList(),
                     AverageRating = Math.Round(p.Reviews.Select(r => r.Rating).ToArray().Average(), 1)
-                }).Where(p=>p.Price<=maxprice).ToList();
+                }).Where(p => p.Price <= maxprice).ToList();
                 return Ok(pricedProducts);
 
             }
@@ -51,31 +53,54 @@ namespace ProductReviewWebAPI.Controllers
                     Rating = r.Rating
 
                 }).ToList(),
-                AverageRating = Math.Round(p.Reviews.Select(r => r.Rating).ToArray().Average(),1)
+                AverageRating = Math.Round(p.Reviews.Select(r => r.Rating).ToArray().Average(), 1)
             }).ToList();
             return Ok(products);
         }
 
-        // GET api/<ProductsController>/5
+        // GET api/Products/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public IActionResult GetProductById(int id)
         {
-            return "value";
+            var product = _context.Products.Select(p => new ProductDTO
+            {
+                Id = p.Id,
+                Name = p.Name,
+                Price = p.Price,
+                Reviews = p.Reviews.Select(r => new ReviewDTO
+                {
+                    Id = r.Id,
+                    Text = r.Text,
+                    Rating = r.Rating
+
+                }).ToList(),
+                AverageRating = Math.Round(p.Reviews.Select(r => r.Rating).ToArray().Average(), 1)
+            }).Where(p => p.Id == id).ToList();
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
         }
 
-        // POST api/<ProductsController>
+        // POST api/Products
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult CreateProduct([FromBody] Product product)
         {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            return StatusCode(201, product);
         }
 
-        // PUT api/<ProductsController>/5
+        // PUT api/Products/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/<ProductsController>/5
+        // DELETE api/Products/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
